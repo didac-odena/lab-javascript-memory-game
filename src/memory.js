742c016d0271ddb4aeb28f1e569195950885f175
Didac Odena <odenadidac@gmail.com>
2025-11-05 18:53:37 +0100

    feat: src/memory.js

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,39 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) return undefined;
+
+    const tempCards = [...this.cards];
+    const randomResult = [];
+
+    while (tempCards.length > 0) {
+      const chosen = Math.floor(Math.random() * tempCards.length);
+      randomResult.push(tempCards[chosen]);
+      tempCards.splice(chosen, 1);
+    }
+
+    this.cards = randomResult;
+    return this.cards;
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2) {
+      this.pairsGuessed++;
+      return true;
+    }
+    return false;
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length / 2) return true;
+    if (this.pairsGuessed === 0) return false;
+    if (this.pairsGuessed > 0) return false;
   }
 }
